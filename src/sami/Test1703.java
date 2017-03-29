6936a6ef38c219ce2d7219d3aaf48fa23d766fd8
sami <sami.torkamani11@yahoo.de>
2017-03-29 19:32:39 +0200

    COMMIT 3

--- src/sami/Test1703.java
+++ src/sami/Test1703.java
@@ -4,7 +4,9 @@ public class Test1703 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Sami Torkamani !!!!");
+		System.out.println("Sami Torkamani :)");
+		System.out.println("Sami Torkamani :)");
+		System.out.println("Sami Torkamani :)");
 	}
 
 }
